e7aa89baa7482a49e59ca8e25c49f776a7952ad8
Ezypoliorz <ezypoliorz.275@gmail.com>
2025-08-21 09:52:15 +0200

    Upload automatique

--- .github/test-raise-error.py
+++ .github/test-raise-error.py
@@ -6,7 +6,9 @@ def main():
         # Assurez-vous d'être dans un dépôt Git
         subprocess.run(['git', 'rev-parse', '--is-inside-work-tree'], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         
-        # Demande à l'utilisateur de saisir le contenu de l'erreur
+        # Le script est conçu pour être exécuté depuis la racine du dépôt Git.
+        # Les chemins ci-dessous sont relatifs à cette racine.
+        
         issue_title = "Test titre erreur 1"
         issue_body = "Test corps de l'erreur 1 !!"
 
@@ -14,20 +16,25 @@ def main():
         os.makedirs('.github', exist_ok=True)
         
         # Crée le fichier déclencheur avec le contenu personnalisé
-        with open('.github/create_issue_trigger', 'w') as f:
+        trigger_file_path = os.path.join('.github', 'error_trigger')
+        with open(trigger_file_path, 'w') as f:
             f.write(f"title: {issue_title}\n\n{issue_body}")
 
-        # Ajoute et commite le fichier
-        subprocess.run(['git', 'add', '.github/create_issue_trigger'], check=True)
-        subprocess.run(['git', 'commit', '-m', f"Signalement d'une erreur: '{issue_title}'"], check=True)
+        # Ajoute le fichier déclencheur au staging area
+        subprocess.run(['git', 'add', trigger_file_path], check=True, capture_output=True, text=True)
+        
+        # Commite le fichier
+        subprocess.run(['git', 'commit', '-m', f"Signalement d'une erreur: '{issue_title}'"], check=True, capture_output=True, text=True)
         
         # Pousse le commit vers le dépôt distant
-        subprocess.run(['git', 'push'], check=True)
+        subprocess.run(['git', 'push'], check=True, capture_output=True, text=True)
 
         print("L'erreur a été signalée avec succès. Une nouvelle erreur a été créée sur le dépôt GitHub.")
-
-    except subprocess.CalledProcessError:
-        print("Erreur: Le script doit être exécuté depuis la racine d'un dépôt Git cloné.")
+    
+    except subprocess.CalledProcessError as e:
+        print(e.stderr)
+        print(e)
+        
     except Exception as e:
         print(f"Une erreur inattendue est survenue: {e}")
 
